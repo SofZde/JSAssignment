80e3211f3730d7b61d64d3fb40d87e252637880a
SofZde <sofzde138@ihmeducation.se>
2024-09-13 10:16:23 +0200

    Update app.js

---
+++ p5Operators/js/app.js
@@ -0,0 +1,40 @@
+let speedinput = 72;
+let speedOutput= speedinput/3.6;
+console.log(speedOutput);
+
+let minutesInput =3;
+let secondsOutput = 3*60;
+console.log(secondsOutput);
+
+let Input1 = 11;
+let Input2= Input1/4;
+console.log(Input2);
+
+let input1= 11;
+let output= input1%4;
+console.log(output);
+
+// A = π r²;
+let cirkleRadius= 2;
+let cirkleArea= (cirkleRadius*cirkleRadius) * 3.14;
+console.log(cirkleArea );
+
+let numberInput=11;
+let negativeOutput= -(numberInput);
+console.log(negativeOutput);
+
+// Input: 70, 1,82 Output: 21,13...
+// Bmi = 1,82* 1,82/ 70
+
+let HeightCm = 1.82;
+let Weight= 70;
+
+let Bmi= Weight/(HeightCm* HeightCm);
+console.log(Bmi);
+
+// a'a + b*b= c*c;
+
+
+
+
+
