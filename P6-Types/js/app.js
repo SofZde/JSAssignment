3ea0d610e0cb4917f65d74713307accc9c76d706
SofZde <sofzde138@ihmeducation.se>
2024-09-16 10:20:12 +0200

    Update app.js

---
+++ P6-Types/js/app.js
@@ -0,0 +1,8 @@
+let numberOfBottles= 10;
+const Line1= "green bottles hanging on the wall ";
+const Line2= "and if one bottle, should accidentally fall, there will be "
+
+
+
+console.log(numberOfBottles, Line1, Line2,  --numberOfBottles)
+
