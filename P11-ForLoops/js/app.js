83b53f06ca99575b7edf6f6b6d3e77e903d67fd9
SofZde <sofzde138@ihmeducation.se>
2024-09-17 10:55:34 +0200

    Update app.js

---
+++ P11-ForLoops/js/app.js
@@ -0,0 +1,20 @@
+/* 13 Random values between 1-6
+
+for (let i=1; i<13; i++) {
+let randomValue = Math.floor(Math.random() * 7);
+console.log(randomValue);
+
+}
+*/
+
+/*for each double value -> print until 1024
+
+for(i=0; i<2048; i*=2)
+{
+console.log(i);
+
+
+}*/
+
+
+
